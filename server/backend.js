ed4b1b3c78a5e0f5e01a59a77336f142e6b0d202
Daniel Haycraft <dhcopy1@gmail.com>
2022-11-01 18:05:18 -0700

    yo

--- server/backend.js
+++ server/backend.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 const port = process.env.PORT || 4005;
 
