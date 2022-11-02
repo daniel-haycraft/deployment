9e46deb232086e3ba9f61b7733f916b8a4fcc896
Daniel Haycraft <dhcopy1@gmail.com>
2022-11-01 17:56:37 -0700

    yo

--- backend.js
+++ backend.js
@@ -7,7 +7,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../public")));
 
-// i don't neex a port number bc heroku does it by itself
+const port = process.env.PORT || 4005;
+
 app.listen(port, ()=> {
     console.log(`Partying on port ${port} till we die`)
 })
